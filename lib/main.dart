25070a4a013b102c340eb6ac5fae7e261f493ac8
opilam <dieudonneopilam2@gmail.com>
2023-04-26 09:15:49 +0200

    first animatiom

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:stock_manager/screen/home.dart';
 
@@ -18,6 +20,58 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       title: 'GESTION-STOCK',
       home: Accueil()
+    );
+  }
+}
+
+class Accueil extends StatefulWidget {
+  const Accueil({super.key});
+
+  @override
+  State<Accueil> createState() => _AccueilState();
+}
+
+class _AccueilState extends State<Accueil> with SingleTickerProviderStateMixin {
+
+    late AnimationController _controller;
+  late Animation _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    _animation = Tween<double>(begin: 0.0, end: 2 * pi).animate(_controller);
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Transform(
+          alignment: Alignment.center,
+          transform: Matrix4.identity()..rotateY(_animation.value),
+          child: Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              color: Colors.blueAccent,
+              borderRadius: BorderRadius.circular(10)
+            ),
+          ),
+        );
+      },
+    ),
+      ),
     );
   }
 }
